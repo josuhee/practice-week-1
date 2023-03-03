917fd768346ba66bc0b508a7104611889e1f9759
Jiyun Kim <adela0603@kyonggi.ac.kr>
2023-03-04 00:05:40 +0900

    실습 1

--- submit/st202111477.c
+++ submit/st202111477.c
@@ -1,5 +1,9 @@
-#include<stdio.h>
-int main(void) {
-	printf("Hello World");
-	return 0;
+#include "week_1"
+void func() {
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = i; j < 5 - i; j++)
+			printf("*");
+		printf("\n");
+	}
 }
