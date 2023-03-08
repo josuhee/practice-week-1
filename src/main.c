60711b10da8066661abcd18099294b0abf400910
josuhee <suheeqwe@gmail.com>
2023-03-08 10:46:24 +0900

    modify main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,5 @@
 #include "week_1.h"
 
-
-void func() {
-	printf("hello World|n");
-}
 int main() {
 	func();
 }
--- src/st202113087.c
+++
@@ -1,9 +0,0 @@
-#include "week_1.h"
-
-
-void func() {
-	printf("hello World|n");
-}
-int main() {
-	func();
-}
